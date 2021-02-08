1a409291ecbefbed75459823234fbf55db66b99b
DanielRzeszutko <daniel.rzeszutko0@gmail.com>
2021-02-08 11:35:19 +0100

    Initialize project

---
+++ src/main/java/com/gymer/GymerApplication.java
@@ -0,0 +1,13 @@
+package com.gymer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gymer/GymerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gymer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
