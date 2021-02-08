910edc8ad00a76ad26eb0821bb4530fa5ab50114
DanielRzeszutko <daniel.rzeszutko0@gmail.com>
2021-02-09 00:42:43 +0100

    Add few endpoints about Slot

--- src/main/java/com/gymer/api/slot/SlotController.java
+++ src/main/java/com/gymer/api/slot/SlotController.java
@@ -7,16 +7,15 @@ import com.gymer.api.slot.entity.SlotDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.Links;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/partners/{partnerId}")
+@RequestMapping("/api/partners/{partnerId}/slots")
 public class SlotController {
 
 	private final SlotService slotService;
@@ -28,13 +27,56 @@ public class SlotController {
 		this.partnerService = partnerService;
 	}
 
-	@GetMapping("/slots")
-	public Iterable<Slot> getAllSlots(@PathVariable Long partnerId) {
+	@GetMapping
+	public Iterable<SlotDTO> getAllSlots(@PathVariable Long partnerId) {
 		Partner partner = partnerService.getPartnerById(partnerId);
-		return partner.getSlots();
+		return partner.getSlots().stream().map(this::convertToSlotDTO).collect(Collectors.toList());
 	}
 
-	//todo implement other endpoints
+	@PostMapping
+	public void addSlotToPartner(@RequestBody SlotDTO slotDTO, @PathVariable Long partnerId) {
+		Partner partner = partnerService.getPartnerById(partnerId);
+		partner.getSlots().add(convertToSlot(slotDTO));
+		partnerService.updatePartner(partner);
+	}
+
+	@GetMapping("/{slotId}")
+	public SlotDTO getSlotById(@PathVariable Long partnerId, @PathVariable Long slotId) {
+		Partner partner = partnerService.getPartnerById(partnerId);
+		List<Slot> slots = partner.getSlots();
+		for (Slot slot : slots) {
+			if (slot.getId().equals(slotId)){
+				return convertToSlotDTO(slot);
+			}
+		}
+		throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+	}
+
+	@PutMapping("/{slotId}")
+	public void updateSlotById(@RequestBody SlotDTO slotDTO, @PathVariable Long partnerId, @PathVariable Long slotId) {
+		if (!slotDTO.getId().equals(slotId)) throw new ResponseStatusException(HttpStatus.CONFLICT);
+
+		Partner partner = partnerService.getPartnerById(partnerId);
+		List<Slot> slots = partner.getSlots();
+		for (Slot slot : slots) {
+			if (slot.getId().equals(slotId)){
+				slotService.updateSlot(convertToSlot(slotDTO));
+			}
+		}
+		throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+	}
+
+	@DeleteMapping("/{slotId}")
+	public void deleteSlot(@PathVariable Long partnerId, @PathVariable Long slotId) {
+		Partner partner = partnerService.getPartnerById(partnerId);
+		List<Slot> slots = partner.getSlots();
+		for (Slot slot : slots) {
+			if (slot.getId().equals(slotId)){
+				slotService.deleteSlot(slot);
+			}
+		}
+		throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+	}
 
 	private SlotDTO convertToSlotDTO(Slot slot) {
 		Links usersLinks = Links.of(slot.getUsers().stream().map(
@@ -47,13 +89,15 @@ public class SlotController {
 				usersLinks,
 				employeeLink,
 				slot.isPrivate()
-
 		);
 	}
 
 	private Slot convertToSlot(SlotDTO slotDTO) {
-		//todo add convert body
-		return null;
+		Slot slot = slotService.getSlotById((slotDTO.getId()));
+		slot.setDate(slotDTO.getDate());
+		slot.setStartTime(slotDTO.getStartTime());
+		slot.setEndTime(slotDTO.getEndTime());
+		return slot;
 	}
 
 }
