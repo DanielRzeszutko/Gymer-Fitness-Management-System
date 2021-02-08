ed5d378304e786e7c0665ebacf666c577216495c
DanielRzeszutko <daniel.rzeszutko0@gmail.com>
2021-02-09 00:42:05 +0100

    Implement Crud methods for Slot.

--- src/main/java/com/gymer/api/slot/SlotService.java
+++ src/main/java/com/gymer/api/slot/SlotService.java
@@ -1,7 +1,10 @@
 package com.gymer.api.slot;
 
+import com.gymer.api.slot.entity.Slot;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class SlotService {
@@ -12,4 +15,18 @@ public class SlotService {
 	public SlotService(SlotRepository slotRepository) {
 		this.slotRepository = slotRepository;
 	}
+
+	public Slot getSlotById(Long slotId) {
+		return slotRepository.findById(slotId).orElseThrow(
+				() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+	}
+
+	public void updateSlot(Slot slot) {
+		slotRepository.save(slot);
+	}
+
+	public void deleteSlot(Slot slot){
+		slotRepository.delete(slot);
+	}
+
 }
