db5ce6a474e24b293813998e0d52869f7c56e74b
Ecoloqy <r2y.szczepaniak@gmail.com>
2021-02-16 12:46:43 +0100

    Add path variable to source of reservation

--- src/main/java/com/gymer/components/reservation/ReservationController.java
+++ src/main/java/com/gymer/components/reservation/ReservationController.java
@@ -6,9 +6,10 @@ import com.gymer.components.reservation.entity.UserReservationDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
-@RequestMapping("/reservation")
+@RequestMapping("/slots/{slotId}/reservation")
 public class ReservationController {
 
     private final ReservationService reservationService;
@@ -20,14 +21,16 @@ public class ReservationController {
 
     @PostMapping("/guest")
     @ResponseStatus(HttpStatus.OK)
-    public JsonResponse reserveAsGuest(@RequestBody GuestReservationDetails details) {
-        return reservationService.updateReservationForGuest(details);
+    public JsonResponse reserveAsGuest(@RequestBody GuestReservationDetails details, @PathVariable Long slotId) {
+        if (details.getSlotId().equals(slotId)) return reservationService.updateReservationForGuest(details);
+        throw new ResponseStatusException(HttpStatus.CONFLICT);
     }
 
     @PostMapping("/user")
     @ResponseStatus(HttpStatus.OK)
-    public JsonResponse reserveAsUser(@RequestBody UserReservationDetails details) {
-        return reservationService.updateReservationForUser(details);
+    public JsonResponse reserveAsUser(@RequestBody UserReservationDetails details, @PathVariable Long slotId) {
+        if (details.getSlotId().equals(slotId)) return reservationService.updateReservationForUser(details);
+        throw new ResponseStatusException(HttpStatus.CONFLICT);
     }
 
 }
