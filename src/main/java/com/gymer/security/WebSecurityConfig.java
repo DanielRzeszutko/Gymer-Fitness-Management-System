6fb5fce0a51137a45da81869088ad69b350d33af
DanielRzeszutko <daniel.rzeszutko0@gmail.com>
2021-02-17 15:15:34 +0100

    Delete in memory User. Create database user authentication.

--- src/main/java/com/gymer/security/WebSecurityConfig.java
+++ src/main/java/com/gymer/security/WebSecurityConfig.java
@@ -1,17 +1,17 @@
 package com.gymer.security;
 
+import com.gymer.components.login.LoginService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.servlet.Filter;
 
@@ -19,29 +19,43 @@ import javax.servlet.Filter;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final LoginService loginService;
     private final String frontUrl;
 
-    public WebSecurityConfig(Environment environment) {
+    public WebSecurityConfig(LoginService loginService, Environment environment) {
+        this.loginService = loginService;
         this.frontUrl = environment.getProperty("server.address.frontend");
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails userDetails = User.withDefaultPasswordEncoder()
-                .username("test").password("test")
-                .roles("USER").build();
-        return new InMemoryUserDetailsManager(userDetails);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeRequests()
-                .antMatchers("/api/partners", "/login").permitAll()
-                .anyRequest().permitAll(); //temporary without authentication
-//                .authenticated()
-//                .and()
-//                .formLogin();
+                .antMatchers("/api/partners",
+                        "/registration/**",
+                        "/js/**",
+                        "/css/**").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .logout().invalidateHttpSession(true).clearAuthentication(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login?logout").permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+        auth.setPasswordEncoder(getPasswordEncoder());
+        auth.setUserDetailsService(loginService);
+        return auth;
     }
 
     @Bean
