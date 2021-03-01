0868fc9adac3a42b5b8f736ee058eabead0e3e7b
Ecoloqy <r2y.szczepaniak@gmail.com>
2021-03-01 14:56:07 +0100

    Add very, very basic package-info.java files into subdirectories in project tree

---
+++ src/main/java/com/gymer/security/common/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package common responsible for holding up all entities, filters and handlers used in security package, from where
+ * every class in security package can get suitable element.
+ *
+ * @author everybody
+ */
+package com.gymer.security.common;
---
+++ src/main/java/com/gymer/security/validation/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package responsible for all validation features. Checking if active user is resource owner,
+ * checking if someone is even logged or browse as guest, and even checking if admin want to manipulate resources.
+ *
+ * @author everybody
+ */
+package com.gymer.security.validation;
