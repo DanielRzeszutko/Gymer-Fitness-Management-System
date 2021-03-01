6f71f49c63f2ebc2b39b681475443f50d8b325f9
Ecoloqy <r2y.szczepaniak@gmail.com>
2021-03-01 15:08:44 +0100

    Add package-info.java file to the main project package

--- src/main/java/com/gymer/common/package-info.java
+++ src/main/java/com/gymer/common/package-info.java
@@ -1,6 +1,6 @@
 /**
- * Classes used to work as common resource repository.
- * Package common contains classes used to get entities from database by jpa repositories,
+ * Classes used more than one time in project.
+ * Package contains classes used to get entities from database by jpa repositories,
  * mailing system with connection to the external mail provider and all entities
  * used commonly in the project.
  *
---
+++ src/main/java/com/gymer/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Project main package. Contains implementation of "Gym and Fitness club management system".
+ *
+ * @author Dominika Jarek
+ * @author Andrzej Szczepaniak
+ * @author Daniel Rzeszutko
+ * @version 1.0
+ */
+package com.gymer;
