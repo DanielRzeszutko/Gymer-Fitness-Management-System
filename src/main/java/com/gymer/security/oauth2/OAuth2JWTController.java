ac894fbbcfe5f2bb2a715e7f64d302e8e448f93e
Ecoloqy <r2y.szczepaniak@gmail.com>
2021-03-02 18:18:17 +0100

    Add OAuth2 JWT getter controller with endpoint for only logged by Google

---
+++ src/main/java/com/gymer/security/oauth2/OAuth2JWTController.java
@@ -0,0 +1,40 @@
+package com.gymer.security.oauth2;
+
+import com.gymer.common.crudresources.credential.CredentialService;
+import com.gymer.common.crudresources.credential.entity.Credential;
+import com.gymer.common.entity.JsonResponse;
+import com.gymer.security.common.entity.AccountDetails;
+import com.gymer.security.login.LoginSuccessHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+public class OAuth2JWTController {
+
+    private final CredentialService credentialService;
+    private final LoginSuccessHandler loginSuccessHandler;
+
+    @GetMapping("/api/google")
+    public void obtainJWTIfLoggedByGoogle(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        try {
+            String userEmail = (String) authentication.getPrincipal();
+            Credential credential = credentialService.getCredentialByEmail(userEmail);
+            AccountDetails accountDetails = new AccountDetails(credential);
+            UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(accountDetails, credential.getPassword());
+            loginSuccessHandler.onAuthenticationSuccess(request, response, authRequest);
+        } catch (IOException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User not logged by Google account.");
+        }
+    }
+
+}
