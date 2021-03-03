a50c62bfedca2bfd93044c395b7a5f6af212246c
Ecoloqy <r2y.szczepaniak@gmail.com>
2021-03-03 21:13:09 +0100

    Reformat code and optimize imports

--- src/main/java/com/gymer/accountlogin/CustomLoginFailureHandler.java
+++ src/main/java/com/gymer/accountlogin/CustomLoginFailureHandler.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
-import org.springframework.web.server.ResponseStatusException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
