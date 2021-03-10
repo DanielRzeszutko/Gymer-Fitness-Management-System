c6c7391b0ccc423b72e7f18cd3f4a064dccd8f69
Ecoloqy <r2y.szczepaniak@gmail.com>
2021-03-10 15:07:34 +0100

    Add simple printing one record from google calendar

--- src/main/java/com/gymer/googlecalendar/GoogleCalendarController.java
+++ src/main/java/com/gymer/googlecalendar/GoogleCalendarController.java
@@ -1,6 +1,5 @@
 package com.gymer.googlecalendar;
 
-import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
@@ -12,25 +11,17 @@ import com.google.api.services.calendar.CalendarScopes;
 import com.google.api.services.calendar.model.Event;
 import com.google.api.services.calendar.model.Events;
 import lombok.RequiredArgsConstructor;
-import org.apache.tomcat.util.http.fileupload.FileUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.OAuth2RefreshToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.List;
@@ -40,7 +31,7 @@ import java.util.List;
 public class GoogleCalendarController {
 
     private final HttpSession session;
-    private final OAuth2AuthorizedClientService clientService;
+    private final OAuth2AuthorizedClientService authClientService;
 
     private static final String APPLICATION_NAME = "Google Calendar API Java Quickstart";
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
@@ -50,26 +41,21 @@ public class GoogleCalendarController {
     public void getGoogleAccount() throws IOException, GeneralSecurityException {
         NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
 
-        InputStream resource = GoogleCalendarController.class.getClassLoader().getResourceAsStream("credentials.json");
-        GoogleCredential credentialDetails = GoogleCredential.fromStream(resource, HTTP_TRANSPORT, JSON_FACTORY).createScoped(SCOPES);
-
-        OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) session.getAttribute("userToken");
-        String clientRegistrationId = oauthToken.getAuthorizedClientRegistrationId();
-        if (clientRegistrationId.equals("google")) {
-            OAuth2AuthorizedClient client = clientService.loadAuthorizedClient(clientRegistrationId, oauthToken.getName());
-            System.out.println(client.getRefreshToken());
+        if (!session.getAttributeNames().hasMoreElements()) {
+            return;
         }
 
-        GoogleCredential credential = new GoogleCredential.Builder()
-                .setTransport(credentialDetails.getTransport())
-                .setJsonFactory(credentialDetails.getJsonFactory())
-                .setServiceAccountId(credentialDetails.getServiceAccountId())
-                .setServiceAccountUser("r2y.szczepaniak@gmail.com")
-                .setServiceAccountPrivateKey(credentialDetails.getServiceAccountPrivateKey())
-                .setServiceAccountScopes(credentialDetails.getServiceAccountScopes())
-                .build();
+        OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) session.getAttribute("userToken");
+        OAuth2AuthorizedClient authClient = authClientService.loadAuthorizedClient(
+                oauthToken.getAuthorizedClientRegistrationId(),
+                oauthToken.getName());
+        OAuth2AccessToken authAccessToken = authClient.getAccessToken();
 
-        Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
+        InputStream resource = GoogleCalendarController.class.getClassLoader().getResourceAsStream("credentials.json");
+        GoogleCredential credentialDetails = GoogleCredential.fromStream(resource, HTTP_TRANSPORT, JSON_FACTORY).createScoped(SCOPES);
+        credentialDetails.setAccessToken(authAccessToken.getTokenValue());
+
+        Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, credentialDetails)
                 .setApplicationName(APPLICATION_NAME)
                 .build();
 
@@ -93,7 +79,7 @@ public class GoogleCalendarController {
                 System.out.printf("%s (%s)\n", event.getSummary(), start);
 
             }
-
         }
+
     }
 }
