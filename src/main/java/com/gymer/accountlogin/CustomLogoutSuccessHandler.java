db8016fe4a9ecd440e44397fda6a10f3e6d9bdf8
Ecoloqy <r2y.szczepaniak@gmail.com>
2021-03-29 12:32:34 +0200

    Removed clear session after logout

--- src/main/java/com/gymer/accountlogin/CustomLogoutSuccessHandler.java
+++ src/main/java/com/gymer/accountlogin/CustomLogoutSuccessHandler.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Component
@@ -19,13 +18,11 @@ import java.io.IOException;
 class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
-    private final HttpSession session;
     private final LanguageComponent language;
 
     @Override
     public void onLogoutSuccess(HttpServletRequest httpServletRequest, HttpServletResponse response, Authentication authentication) throws IOException {
         final boolean IS_ERROR = false;
-        session.setAttribute("userToken", null);
         SecurityContextHolder.getContext().setAuthentication(null);
         response.setStatus(HttpStatus.OK.value());
         response.setContentType("application/json");
