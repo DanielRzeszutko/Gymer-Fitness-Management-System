8804919c3320fc24198b9e2885487fbc53a70b1f
Ecoloqy <r2y.szczepaniak@gmail.com>
2021-03-29 12:34:06 +0200

    Add method that creates token from OAuth2 object

--- src/main/java/com/gymer/commoncomponents/jwtcreator/JWTCreatorComponent.java
+++ src/main/java/com/gymer/commoncomponents/jwtcreator/JWTCreatorComponent.java
@@ -2,9 +2,12 @@ package com.gymer.commoncomponents.jwtcreator;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -14,6 +17,7 @@ import java.util.Date;
 public class JWTCreatorComponent {
 
     private final Environment environment;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     public String createToken(Authentication authentication) {
         String starter = "Bearer ";
@@ -28,6 +32,19 @@ public class JWTCreatorComponent {
         return tokenStarter + JWT.create()
                 .withSubject(authentication.getName())
                 .withClaim("roles", String.valueOf(authentication.getAuthorities()))
+                .withIssuedAt(new Date(now))
+                .withExpiresAt(new Date(now + 864000000))
+                .sign(algorithm);
+    }
+
+    public String createOAuth2Token(OAuth2AuthenticationToken authentication) throws JsonProcessingException {
+        String secretKey = environment.getProperty("jwt.secret.password");
+        secretKey = secretKey != null ? secretKey : "";
+        Algorithm algorithm = Algorithm.HMAC512(secretKey);
+        long now = System.currentTimeMillis();
+        String authenticationObject = objectMapper.writeValueAsString(authentication);
+        return JWT.create()
+                .withSubject(authenticationObject)
                 .withIssuedAt(new Date(now))
                 .withExpiresAt(new Date(now + 864000000))
                 .sign(algorithm);
